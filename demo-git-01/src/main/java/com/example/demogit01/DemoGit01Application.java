5ef93fff0076be99a0e2fbfc81a58f3ce23b2ee0
Administrator <admin@example.com>
2019-09-04 11:43:55 +0800

    first commit project

---
+++ demo-git-01/src/main/java/com/example/demogit01/DemoGit01Application.java
@@ -0,0 +1,13 @@
+package com.example.demogit01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGit01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGit01Application.class, args);
+	}
+
+}
